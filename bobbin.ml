6cfea1297f62dd6cf35c84bb470d92c6ef5e292d
Phil Gekni <phil@Utgard.(none)>
2013-01-15 18:25:32 +0100

    Mis a jour du moteur son: -Ajout d'une fonction playSample qui joue un bruitage en WMA. -Ajout d'une fonction playMusic aui joue une musique sur le canal principal. -Ajout d'une fonction lowSound (buguee pour le moment) qui diminuera le son d'un pourcentage donnee d'une duree donnee pendant une duree donnee. -Ajout de InitBobbin et closeBobbin. -Ajout de diverses fonctions test qui serviront a la soutenance.

---
+++ bobbin.ml
@@ -0,0 +1,58 @@
+open Sdlmixer
+
+let initBobbin () =
+	open_audio ()
+;;
+
+let closeBobbin () =
+	close_audio ()
+;;
+
+let playMusic path =
+	let file = load_music path in
+	play_music file
+;;
+
+
+let lowSound percentage delay channel =
+	let a = (int_of_float (volume_channel channel)) in
+	let b = a*percentage/100 in
+	setvolume_channel channel (float_of_int b);
+	Sdltimer.delay(delay);
+	setvolume_channel channel (float_of_int a)
+;;
+(*
+let channelplay path =
+	allocate_channels 3;
+	play_channel 1 2 23  path;;
+	
+	*)
+
+let gogogo () =
+	allocate_channels 2
+;;
+
+let test () = 
+	let son = loadWAV "appl.WAV" in
+	play_channel son
+;;
+
+let playSample sound = 
+	let son = loadWAV sound in
+	play_channel son
+;;
+
+let lowSound ?retard:(r = 0) percentage delay =
+	Sdltimer.delay(r);
+	let a = (int_of_float (volume_channel 0)) in
+	let b = a*percentage/100 in
+	setvolume_channel 0 (float_of_int b);
+	Sdltimer.delay(delay);
+	setvolume_channel 0 (float_of_int a)
+;;
+
+let tist () = 
+	let y = 3000 in
+	test();
+	lowSound ~retard:y 80 1000
+;;
