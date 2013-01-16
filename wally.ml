b427ac14f6eebdcacb609c29d1cca08de164e9af
Yayg <rafael.gozlan@wanadoo.fr>
2013-01-16 12:37:53 +0100

    Correction de l'erreur de pile vide (cf la methode endParsing).

--- wally.ml
+++ wally.ml
@@ -98,8 +98,10 @@ class treeXml xmlFile =
 					| [] -> dict
 				in Element(name, browser (new dictionary) attrs)
 			in 
-			push (Node(element, VoidTree, VoidTree)) stack
-		method private pushText stack text = 
+			push (Node(element, VoidTree, VoidTree)) stack;
+			print_string ("pushStartElement : "^name^"\n")
+		method private pushText stack text =
+			print_string ("pushText : "^text^"\n");
 			push (Node(Text(text), VoidTree, VoidTree)) stack
 		method private pushEndElement stack name =
 			let getNameElement node = 
@@ -128,13 +130,12 @@ class treeXml xmlFile =
 			
 			in 
 			while getNameElement !currentNode <> name do
-				currentNode := setBrotherTree !previousNode !currentNode;
-				previousNode := !currentNode;
-				
-				currentNode := pop stack
+				previousNode := (setBrotherTree !previousNode !currentNode);
+				currentNode := (pop stack)
 			done;
 			currentNode := setChildrenTree !previousNode !currentNode;
-			push !currentNode stack
+			push !currentNode stack;
+			print_string ("pushEndElement : "^name^"\n")
 		method private endParsing stack =
 			let setBrotherTree brother = function
 				| VoidTree -> 
@@ -146,10 +147,9 @@ class treeXml xmlFile =
 			and currentNode = ref (pop stack)
 			
 			in
-			while is_empty stack do
+			while not (is_empty stack) do
 				previousNode := !currentNode;
 				currentNode := pop stack;
-				
 				currentNode := setBrotherTree !previousNode !currentNode
 			done;
 			data <- !currentNode
